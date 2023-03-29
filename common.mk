Sexynos/device_samsung_exynos9810-common

exynos9810-common: usb: rebrand and switch to AIDL service

common.mk
@@ -358,7 +358,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.exynos9810
+    android.hardware.usb-service.exynos9810
 
 # Vibrator
 PRODUCT_PACKAGES += \

phence/android_device_samsung_exynos9810-common

Change-Id: Ifd3081f790933dd9dadea06a39b406d45479a85a

common.mk
@@ -309,7 +309,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.samsung
+    android.hardware.usb-service.samsung
 
 # Vibrator
 PRODUCT_PACKAGES += \